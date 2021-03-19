9ec7983f8d9080b81c1d86387f920a3f8eb342f0
junyeongCHOI <asxd153@gmail.com>
2021-03-19 18:59:07 +0900

    add card mock

--- components/ColorCard/ColorCard.tsx
+++ components/ColorCard/ColorCard.tsx
@@ -1,5 +1,26 @@
 import styles from "./ColorCard.module.scss";
+import moment from "moment";
 import { Color } from "../../pages/api/colors";
+
+moment.updateLocale("en", {
+  relativeTime: {
+    past: "%s ago",
+    s: "Today",
+    ss: "Today",
+    m: "Today",
+    mm: "Today",
+    h: "Today",
+    hh: "Today",
+    d: "Yesterday",
+    dd: "%d days",
+    w: "a week",
+    ww: "%d weeks",
+    M: "a month",
+    MM: "%d months",
+    y: "a year",
+    yy: "%d years",
+  },
+});
 
 interface ColorCardProps {
   data: Color;
@@ -14,7 +35,7 @@ const ColorCard = ({ data }: ColorCardProps) => {
           <i className="xi-heart" />
           {data.likes}
         </div>
-        <div className={styles.date}>{data.date}</div>
+        <div className={styles.date}>{moment(data.date).fromNow(true)}</div>
       </div>
     </div>
   );
