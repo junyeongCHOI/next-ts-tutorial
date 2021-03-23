adf389ea47e3133a183bd7fa5976bd1cffff5a2d
junyeongCHOI <asxd153@gmail.com>
2021-03-23 17:08:18 +0900

    add jest

--- utils/colorSlicer.test.ts
+++ utils/colorSlicer.test.ts
@@ -1,9 +1,30 @@
 import colorSlicer from "./colorSlicer";
 
-describe("[Func] colorSlicer", () => {
-  test("6자리씩 분해 후 #을 붙여 배열을 리턴", () => {
+describe("6자리씩 분해 후 #을 붙여 배열을 리턴", () => {
+  test("24자리", () => {
     const colors: string = "000000111111222222333333";
     const result: string[] = ["#000000", "#111111", "#222222", "#333333"];
+
+    expect(colorSlicer(colors)).toEqual(result);
+  });
+
+  test("14자리", () => {
+    const colors: string = "00000011111122";
+    const result: string[] = ["#000000", "#111111", "#22"];
+
+    expect(colorSlicer(colors)).toEqual(result);
+  });
+
+  test("0자리", () => {
+    const colors: string = "";
+    const result: string[] = [];
+
+    expect(colorSlicer(colors)).toEqual(result);
+  });
+
+  test("문자열이 아닌 경우", () => {
+    const colors: undefined = undefined;
+    const result: string[] = [];
 
     expect(colorSlicer(colors)).toEqual(result);
   });
--- utils/colorSlicer.ts
+++ utils/colorSlicer.ts
@@ -1,6 +1,6 @@
 const colorSlicer = (string: string): string[] => {
   const result: string[] = [];
-  const amount: number = string.length / 6;
+  const amount: number = typeof string === "string" ? string.length / 6 : 0;
 
   for (let i = 0; i < amount; i++) {
     result.push("#" + string.slice(i * 6, i * 6 + 6));
